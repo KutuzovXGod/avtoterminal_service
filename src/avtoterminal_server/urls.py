19b517ed5fa0525d0bbbe7bbca1708cc7c3664b3
Степан Кутузов <stepan.kutuzovx27@gmail.com>
2023-10-04 16:48:32 +0300

    version 1.3 store page

--- src/avtoterminal_server/urls.py
+++ src/avtoterminal_server/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main_app.views import base_view
+from main_app.views import base_view, products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', base_view, name='base_view'),
+    path('products/', products, name='products'),
 ]
--- src/main_app/views.py
+++ src/main_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def base_view(request):
     return render(request, 'main_app/index.html')
+
+
+def products(request):
+    return render(request, 'main_app/products.html')
