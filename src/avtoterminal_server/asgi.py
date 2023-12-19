01bb23f6b6859d4dd89552c06c3ef45c5d8c8ba1
Степан Кутузов <stepan.kutuzovx27@gmail.com>
2023-12-19 13:03:25 +0300

    исправлен баг белой стороны на мобильных устройствах

--- src/avtoterminal_server/asgi.py
+++ src/avtoterminal_server/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for avtoterminal project.
+ASGI config for avtoterminal_server project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avtoterminal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avtoterminal_server.settings')
 
 application = get_asgi_application()
